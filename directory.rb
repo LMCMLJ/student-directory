c4cd3f770f408fb954a66648867e797448259c07
LMCMLJ <mjcalvey2@gmail.com>
2016-09-14 17:40:20 +0100

    Changed datastructure to hash to allow more detail
    The datastructure for students has been changed so that multiple parameters
    can be included in the future. At the moment, only name and cohort are used.

--- directory.rb
+++ directory.rb
@@ -3,16 +3,16 @@
 #created by Michael Calvey, Sept 2016
 
 students = [
-  'Darth Vader',
-  'Nurse Ratched',
-  'Michael Corleone',
-  'Alex DeLarge',
-  'The Wicked Witch of the West',
-  'Terminator',
-  'Freddy Kreuger',
-  'The Joker',
-  'Joffrey Baratheon',
-  'Norman Bates'
+  {name: 'Darth Vader', cohort: :november},
+  {name: 'Nurse Ratched', cohort: :november},
+  {name: 'Michael Corleone', cohort: :november},
+  {name: 'Alex DeLarge', cohort: :november},
+  {name: 'The Wicked Witch of the West', cohort: :november},
+  {name: 'Terminator', cohort: :november},
+  {name: 'Freddy Kreuger', cohort: :november},
+  {name: 'The Joker', cohort: :november},
+  {name: 'Joffrey Baratheon', cohort: :november},
+  {name: 'Norman Bates', cohort: :november}
 ]
 
 def print_header
@@ -20,9 +20,9 @@ def print_header
   puts "______________"
 end
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
