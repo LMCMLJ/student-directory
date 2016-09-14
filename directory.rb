f9c0e1adc764e4217bab4366c91f4a3d876e652c
LMCMLJ <mjcalvey2@gmail.com>
2016-09-14 16:46:10 +0100

    Added students as an array, and made number of students a function of said array

--- directory.rb
+++ directory.rb
@@ -2,22 +2,18 @@
 #version 0.1
 #created by Michael Calvey, Sept 2016
 
-#first we print the list of students
-puts "The students of Villains Academy"
-puts "_________________"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Kreuger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#finally we print the total number of students
-print "Overall, we have "
-print 11
-puts " great students"
+students = ['Darth Vader',
+            'Nurse Ratched',
+            'Michael Corleone',
+            'Alex DeLarge',
+            'The Wicked Witch of the West',
+            'Terminator',
+            'Freddy Kreuger',
+            'The Joker',
+            'Joffrey Baratheon',
+            'Norman Bates'
+]
+student_count = students.length
+puts "Here are the students of Villains Academy: #{students}"
+puts "Overall, there are #{student_count} students"
 
