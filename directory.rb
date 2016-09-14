bc0b3712aa677418ec2302857477732815e48f4e
LMCMLJ <mjcalvey2@gmail.com>
2016-09-14 17:16:58 +0100

    Added methods to print header, names and footer

--- directory.rb
+++ directory.rb
@@ -2,18 +2,34 @@
 #version 0.1
 #created by Michael Calvey, Sept 2016
 
-students = ['Darth Vader',
-            'Nurse Ratched',
-            'Michael Corleone',
-            'Alex DeLarge',
-            'The Wicked Witch of the West',
-            'Terminator',
-            'Freddy Kreuger',
-            'The Joker',
-            'Joffrey Baratheon',
-            'Norman Bates'
+students = [
+  'Darth Vader',
+  'Nurse Ratched',
+  'Michael Corleone',
+  'Alex DeLarge',
+  'The Wicked Witch of the West',
+  'Terminator',
+  'Freddy Kreuger',
+  'The Joker',
+  'Joffrey Baratheon',
+  'Norman Bates'
 ]
-student_count = students.length
-puts "Here are the students of Villains Academy: #{students}"
-puts "Overall, there are #{student_count} students"
 
+def print_header
+  puts "The students of Villains Academy"
+  puts "______________"
+end
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
