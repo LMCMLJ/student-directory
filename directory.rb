0eeab7f9f6c400eedcc53427b7bed1cc9b60457b
LMCMLJ <mjcalvey2@gmail.com>
2016-09-16 14:30:05 +0100

    Reverting to old print method for now

--- directory.rb
+++ directory.rb
@@ -21,8 +21,8 @@ def print_header
 end
 
 def print(students)
-  students.to_enum.with_index(1).each do |elem, i|
-    puts "#{i}: #{students[name]} (#{students[cohort]} cohort)"
+  students.to_enum.with_index(1).each do |student, i|
+    puts "#{i}: #{student[name]} (#{student[cohort]} cohort)"
   end
 end
 
