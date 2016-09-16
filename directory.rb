c3e96eaac6ba0900d4ec7228e6de26bbd8aaf888
LMCMLJ <mjcalvey2@gmail.com>
2016-09-16 14:08:50 +0100

    Temp commit, testing new directory method

--- directory.rb
+++ directory.rb
@@ -21,8 +21,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.to_enum.with_index(1).each do |elem, i|
+    puts "#{i}: #{students[name]} (#{students[cohort]} cohort)"
   end
 end
 
